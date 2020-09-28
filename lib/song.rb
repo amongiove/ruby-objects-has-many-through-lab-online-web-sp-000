39dd264623fed0a0ba596ffa3829185bc57b143f
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-28 22:37:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist, genre)
+  def initialize(name)
     @name = name
     @artist = artist
     @genre = genre
