36f1f7318ca0172a2d5769b86a66b52c9cf32dca
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-28 22:47:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,12 @@ class Artist
     @@all
   end
 
-  def new_song(name, genre)
-    Song.new(name, self, genre)
-  end
+  def songs
+    Song.all.select {|song| song.artist == self}
+
+  # def new_song(name, genre)
+  #   Song.new(name, self, genre)
+  # end
 
 
 
